b36b5aa28771103ae03cd638aeb40343736e5aae
Hamsims <hamsims09876@gmail.com>
2026-04-22 04:09:50 +0700

    fix: update CapsuleCard link to include dynamic route and enable CapsuleGrid component in Landing page

--- src/components/CapsuleCard.jsx
+++ src/components/CapsuleCard.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function CapsuleCard({ item }) {
   return (
-    <Link className="card" to={`/feature/${item.key}`}>
+    <Link className="card" >
       <div className="card-title">{item.title}</div>
       <div className="card-desc">{item.desc}</div>
     </Link>
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -85,7 +85,7 @@ export default function Home() {
           </div>
         </div>
 
-        {/* <CapsuleGrid /> */}
+        <CapsuleGrid />
       </main>
     </div>
   );
